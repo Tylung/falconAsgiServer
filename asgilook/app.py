1468890f955d3f8943875b9ad6529ebe0dd968de
Jona <jonaenglish64@gmail.com>
2022-07-27 15:54:03 -0500

    agregando middleware redis al app.py

--- asgilook/app.py
+++ asgilook/app.py
@@ -2,29 +2,31 @@ import falcon.asgi
 
 # app = falcon.asgi.App()
 
-import falcon.asgi
-
 from .config import Config
+from .cache import RedisCache
 from .images import Images, Thumbnails
 from .store import Store
 from .home import Home
 
 
+
 def create_app(config=None):
     config = config or Config()
+    cache = RedisCache(config)
     store = Store(config)
     images = Images(config, store)
     home = Home()
     thumbnails = Thumbnails(store)
 
-    app = falcon.asgi.App()
+    app = falcon.asgi.App(middleware=[cache])
+    app.add_route('/', home)
     app.add_route('/images', images)
     app.add_route('/images/{image_id:uuid}.jpeg', images, suffix='image')
     app.add_route(
         '/thumbnails/{image_id:uuid}/{width:int}x{height:int}.jpeg',
         thumbnails
     )
-    app.add_route('/', home)
+
 
     return app
     
